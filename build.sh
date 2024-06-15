e65e1e011a977a63f71672ea6e63ec317d971147
Juanan242 <barberojuanan@gmail.com>
2024-06-15 23:34:30 +0200

    Fix build.sh script and workflow

--- build.sh
+++ build.sh
@@ -3,10 +3,17 @@
 # Asegurarse de estar en el directorio del proyecto
 cd $GITHUB_WORKSPACE/YouTubeDownloaderBPM
 
+# Instalar Python y pip si no están disponibles
+if ! command -v python3 &> /dev/null; then
+    echo "Python3 no está instalado. Instalando Python3..."
+    sudo apt update
+    sudo apt install -y python3 python3-pip
+fi
+
 # Verificar si pyinstaller está disponible
 if ! command -v pyinstaller &> /dev/null; then
     echo "pyinstaller no está instalado. Instalando pyinstaller..."
-    pip install pyinstaller
+    pip3 install pyinstaller
 fi
 
 # Verificar que el archivo main.py existe en la ubicación esperada
@@ -28,4 +35,4 @@ fi
 mkdir -p $GITHUB_WORKSPACE/YouTubeDownloaderBPM-Binaries/
 
 # Mover el ejecutable generado al directorio de binarios
-mv dist/main $GITHUB_WORKSPACE/YouTubeDownloaderBPM-Binaries/main.exe
+mv dist/main $GITHUB_WORKSPACE/YouTubeDownloaderBPM-Binaries/
