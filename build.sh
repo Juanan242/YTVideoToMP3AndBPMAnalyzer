a4ecaaef404f822d570376361140403e7bbafa22
Juanan242 <barberojuanan@gmail.com>
2024-06-15 23:03:01 +0200

    Fix build.sh script and workflow

--- build.sh
+++ build.sh
@@ -22,6 +22,12 @@ fi
 # Configurar wine para usar una implementación sin interfaz gráfica
 export DISPLAY=:0
 
+# Verificar si pyinstaller está disponible
+if ! command -v pyinstaller &> /dev/null; then
+    echo "pyinstaller no está instalado. Instalando pyinstaller..."
+    pip3 install pyinstaller
+fi
+
 # Generar el ejecutable utilizando PyInstaller y Wine
 wine pyinstaller --onefile Escritorio/YouTubeDownloaderBPM/main.py
 
@@ -30,6 +36,9 @@ if [ ! -f Escritorio/YouTubeDownloaderBPM/dist/main.exe ]; then
     echo "Error: No se pudo encontrar el archivo main.exe en la ubicación esperada."
     exit 1
 fi
+
+# Crear el directorio de binarios si no existe
+mkdir -p $GITHUB_WORKSPACE/Escritorio/YouTubeDownloaderBPM-Binaries/
 
 # Mover el ejecutable generado al directorio de binarios
 mv Escritorio/YouTubeDownloaderBPM/dist/main.exe $GITHUB_WORKSPACE/Escritorio/YouTubeDownloaderBPM-Binaries/
