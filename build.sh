ba7f78a702ebeb98775d3e4b5f9e1a60e705bee4
Juanan242 <barberojuanan@gmail.com>
2024-06-15 23:07:10 +0200

    Fix build.sh script and workflow

--- build.sh
+++ build.sh
@@ -3,37 +3,18 @@
 # Asegurarse de estar en el directorio del proyecto
 cd $GITHUB_WORKSPACE
 
-# Verificar si wine está disponible
-if ! command -v wine &> /dev/null; then
-    echo "wine no está instalado. Instalando wine..."
-    # Instalar wine si no está disponible
-    if [ "$(uname -m)" = "x86_64" ]; then
-        sudo dpkg --add-architecture i386
-    fi
-    sudo apt update
-    sudo apt install -y wine64 wine32
-    # Verificar si la instalación fue exitosa
-    if ! command -v wine &> /dev/null; then
-        echo "Error: No se pudo instalar wine correctamente."
-        exit 1
-    fi
-fi
-
-# Configurar wine para usar una implementación sin interfaz gráfica
-export DISPLAY=:0
-
 # Verificar si pyinstaller está disponible
 if ! command -v pyinstaller &> /dev/null; then
     echo "pyinstaller no está instalado. Instalando pyinstaller..."
     pip3 install pyinstaller
 fi
 
-# Generar el ejecutable utilizando PyInstaller y Wine
-wine pyinstaller --onefile Escritorio/YouTubeDownloaderBPM/main.py
+# Generar el ejecutable utilizando PyInstaller
+pyinstaller --onefile Escritorio/YouTubeDownloaderBPM/main.py
 
 # Verificar si se generó correctamente main.exe
-if [ ! -f Escritorio/YouTubeDownloaderBPM/dist/main.exe ]; then
-    echo "Error: No se pudo encontrar el archivo main.exe en la ubicación esperada."
+if [ ! -f Escritorio/YouTubeDownloaderBPM/dist/main ]; then
+    echo "Error: No se pudo encontrar el archivo main en la ubicación esperada."
     exit 1
 fi
 
@@ -41,4 +22,4 @@ fi
 mkdir -p $GITHUB_WORKSPACE/Escritorio/YouTubeDownloaderBPM-Binaries/
 
 # Mover el ejecutable generado al directorio de binarios
-mv Escritorio/YouTubeDownloaderBPM/dist/main.exe $GITHUB_WORKSPACE/Escritorio/YouTubeDownloaderBPM-Binaries/
+mv Escritorio/YouTubeDownloaderBPM/dist/main $GITHUB_WORKSPACE/Escritorio/YouTubeDownloaderBPM-Binaries/main.exe
