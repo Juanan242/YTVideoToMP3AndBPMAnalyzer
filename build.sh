23e34777305d948b23d910b28de3fca86bcb9888
Juanan242 <barberojuanan@gmail.com>
2024-06-15 20:51:02 +0200

    fixing .yml and deleting .sh

--- build.sh
+++
@@ -1,9 +0,0 @@
-cd ~/Escritorio/YouTubeDownloaderBPM
-
-# Generar el ejecutable utilizando PyInstaller y Wine
-wine pyinstaller --onefile main.py
-
-# Mover el ejecutable generado al directorio de binarios
-mv dist/main.exe Escritorio/YouTubeDownloaderBPM-Binaries/
-
-gh release upload v1 Escritorio/YouTubeDownloaderBPM-Binaries/main.exe
