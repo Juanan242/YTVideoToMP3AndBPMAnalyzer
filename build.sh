ff0604b940812807beda55106395d43cf2e41e77
Juanan242 <barberojuanan@gmail.com>
2024-06-15 21:27:14 +0200

    Fix build.sh script and workflow

--- build.sh
+++ build.sh
@@ -1,7 +1,10 @@
-cd Escritorio/YouTubeDownloaderBPM/
+#!/bin/bash
+
+# Cambiar al directorio del proyecto
+cd $GITHUB_WORKSPACE/Escritorio/YouTubeDownloaderBPM/
 
 # Generar el ejecutable utilizando PyInstaller y Wine
 wine pyinstaller --onefile main.py
 
 # Mover el ejecutable generado al directorio de binarios
-mv dist/main.exe /Escritorio/YouTubeDownloaderBPM-Binaries/
+mv dist/main.exe $GITHUB_WORKSPACE/Escritorio/YouTubeDownloaderBPM-Binaries/
